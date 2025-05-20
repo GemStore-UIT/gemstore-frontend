2fcd7f9e76c61db9a51279512efa1e88fb3d3fec
QUOC HAI <haiquocntp@gmail.com>
2025-05-20 22:11:09 +0700

    feat: wireframe for home screen

---
+++ lib/config/router.dart
@@ -0,0 +1,25 @@
+// import 'package:go_router/go_router.dart';
+
+// class RouteName {
+//   static const String home = '/home';
+//   static const String login = '/login';
+// }
+
+// final router = GoRouter(
+//   initialLocation: RouteName.login,
+//   routes: [
+//     GoRoute(
+//       path: RouteName.login,
+//       builder: (context, state) {
+//         return const LoginScreen();
+//       },
+//     ),
+//     GoRoute(
+//       path: RouteName.home,
+//       builder: (context, state) {
+//         return const HomeScreen();
+//       },
+//     ),
+//   ],
+// );
+
